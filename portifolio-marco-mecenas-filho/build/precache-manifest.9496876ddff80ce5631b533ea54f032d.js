86093826fa070e6cbb1ece93bf1b04102244813b
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-01-24 22:17:31 -0300

    portifolio 2

---
+++ portifolio-marco-mecenas-filho/build/precache-manifest.9496876ddff80ce5631b533ea54f032d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbee507ba3cbbbfacf68dad3c5fceef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83f514dacac9b16b3a13",
+    "url": "/static/css/2.945f44db.chunk.css"
+  },
+  {
+    "revision": "19d9bf0e178e1484bec6",
+    "url": "/static/css/main.a6c826dd.chunk.css"
+  },
+  {
+    "revision": "83f514dacac9b16b3a13",
+    "url": "/static/js/2.4a049b68.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.4a049b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d9bf0e178e1484bec6",
+    "url": "/static/js/main.5a7712d3.chunk.js"
+  },
+  {
+    "revision": "8f2579ff55bd5aa1997a",
+    "url": "/static/js/runtime-main.87831706.js"
+  },
+  {
+    "revision": "e73cb5d3d2cf0169d10e958e2cd8a0a8",
+    "url": "/static/media/home.e73cb5d3.gif"
+  },
+  {
+    "revision": "26b83b5748759344650fbe3699382ffc",
+    "url": "/static/media/marcomecenasfilho-curriculo.26b83b57.pdf"
+  },
+  {
+    "revision": "a1ebb3689dc26712f655fa8e90349040",
+    "url": "/static/media/minhafoto2.a1ebb368.jpg"
+  },
+  {
+    "revision": "736aeafb595367693ce484d309eb416e",
+    "url": "/static/media/playtunes.736aeafb.gif"
+  },
+  {
+    "revision": "976a0965bd285ac8a7e9d561c7fcf922",
+    "url": "/static/media/receitasshowshow.976a0965.gif"
+  },
+  {
+    "revision": "4e08136842cd4ccfcad41ef38bcb5616",
+    "url": "/static/media/wallet.4e081368.gif"
+  }
+]);
