bdd81ee52391a847f21c5832d94de63d2e003fb3
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-02-21 10:53:01 -0300

    typing animation

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,14 +6,14 @@ export default function AboutMe() {
   return (
     <div className='about-me'>
       <div className='about-div'>
-        <img src={minhafoto2} width="300px" alt="marco-foto"/>
-        <article className='text-aboutme'>
+        <img src={minhafoto2} className='animate__animated animate__pulse animate__infinite	' width="300px" alt="marco-foto"/>
+        <article className='text-aboutme animate__animated  animate__fadeIn'>
         <p> Olá, meu nome é Marco Antonio. Brasileiro e residente na cidade de São 
           José dos Campos/SP. Atualmente estou em transição de carreira. Partindo da área Química
           para de Tecnologia. Sou estudante <strong> Full Stack de desenvolvimento web na
           @Trybe</strong>. Possuo conhecimento e experiência com Desenvolvimento <strong>Front-end
           </strong>, utilizando as tecnologias <strong>Git, CSS3, HTML5, JavaScripitES6, ReactJs
-          (class e functions), ContextAPI, Hooks, Redux e BootStrap</strong>.
+          (class e functions), ContextAPI, Hooks, Redux, SQL, NodeJs e BootStrap</strong>.
           </p>
           <p> Formado em <strong>Química com Mestrado em Agroquímica</strong>. Possuo conhecimentos em sistema SAP e Lean
           Six Sigma (Yellow belt). Possuo experiência em <strong>desenvolvimento de projetos de pesquisa, gestão
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../style/home.css'
 import home from '../images/home.gif'
+import 'animate.css';
 
 export default function Home() {
   return <div className='home-container'>
-    <div className='home-align'>
-      <img src={ home } alt='logo' />
+    <div className='home-align ' >
+      <img src={ home }  alt='logo' />
       <div className='home-text'>
         <h1>Olá, tudo bom? </h1>
-        <p>Sou o Marco Mecenas Filho</p>
-        <p>Desenvolvedor Full-Stack</p>
+        <p className='type-simple'>Sou o <span className='span-name'>Marco Mecenas Filho</span></p>
+        <p className='type-simple'>Desenvolvedor <span className='span-name'> Full-Stack</span></p>
         <p>Seja bem vindo(a) ao meu portifólio</p>
-        <h5>Teve uma grande ideia e não sabe como fazer ? Eu posso te ajudar</h5>
+        <h5>Teve uma grande ideia e não sabe como fazer ? </h5>
+        <h5 className='type-simple span-name'> Eu posso te ajudar</h5>
       </div>
     </div>
   </div>;
