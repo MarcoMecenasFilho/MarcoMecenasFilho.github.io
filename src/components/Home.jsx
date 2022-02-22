fce40382ec56770662abcf77ede9c07bcab11790
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-02-22 10:21:43 -0300

    mobile layout fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         <p className='type-simple'>Sou o <span className='span-name'>Marco Mecenas Filho</span></p>
         <p className='type-simple '>Desenvolvedor <span className='span-name'> Full-Stack</span></p>
         <p>Seja bem vindo(a) ao meu portifólio</p>
-        <h5>Teve uma grande ideia e não sabe como fazer ? </h5>
+        <h5 className='h5-text'>Teve uma grande ideia e não sabe como fazer ? </h5>
         <h5 className='type-simple  span-name'> Eu posso te ajudar</h5>
       </div>
     </div>
